cb910dfb49b6a429eac013182391fbba037844ae
Harry Li <hualili@yahoo.com>
2018-07-20 17:05:17 -0700

    Add files via upload

--- IP110-Summer18/kmean2.cpp
+++ IP110-Summer18/kmean2.cpp
@@ -1,5 +1,5 @@
 /*-----------------------------------------------------------------------------------* 
- *  Program: kmean2.cpp                                                               *
+ *  Program: kmean.cpp                                                               *
  *  Coded by: OpenCV samples           Date: Jul 18, 18                              *
  *  Version: x01.0                     Status: tested                                *
  *  Purpose:                                                                         *  
@@ -18,23 +18,24 @@ int main( int /*argc*/, char** /*argv*/ )
  cout << "\n double kmeans(const Mat& samples, int clusterCount, Mat& labels, TermCriteria termcrit, int attempts, int flags, Mat* centers) \n\n\n" << endl;
   
  int clusterCount = 2;
- int dimensions = 4;   //5
- int sampleCount = 50;
+ int dimensions = 3;   //5
+ int sampleCount = 4;
  
- Mat points(sampleCount,dimensions, CV_32F,Scalar(10));
+ Mat points(sampleCount,dimensions, CV_32F,Scalar(0));
  Mat labels;
  Mat centers(clusterCount, 1, points.type());
-
- // values of 1st half of data set is set to 10
- //change the values of 2nd half of the data set; i.e. set it to 20
  
- for(int i =24;i<points.rows;i++)
- {
-  for(int j=0;j<points.cols;j++)
-  {
-   points.at<float>(i,j)=20;
-  }
- }
+   // 1 feature vector  
+   points.at<float>(0,0)=10; points.at<float>(0,1)=20; points.at<float>(0,2)=20;
+
+   // 2nd feature vector 
+   points.at<float>(1,0)=0;  points.at<float>(1,1)=0; points.at<float>(1,2)=0;
+
+   // 3rd feature vector 
+   points.at<float>(2,0)=0;  points.at<float>(2,1)=0; points.at<float>(2,2)=0;
+
+   // 4th feature vector 
+   points.at<float>(3,0)=5;  points.at<float>(3,1)=4; points.at<float>(3,2)=4;
  
  kmeans(points, clusterCount, labels, TermCriteria( CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 10, 1.0), 3, KMEANS_PP_CENTERS, centers);
     // we can print the matrix directly.
