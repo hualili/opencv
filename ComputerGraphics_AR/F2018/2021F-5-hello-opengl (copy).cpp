d6455fd06431f3cc2c1ea77cda9cd4217eea449e
Harry Li <hualili@yahoo.com>
2021-09-02 16:56:51 -0700

    Add files via upload

---
+++ ComputerGraphics_AR/F2018/2021F-5-hello-opengl (copy).cpp
@@ -0,0 +1,34 @@
+/*-------------------------------------------------------------------------*
+* Program: hello_opengl.cpp;         Coded by: HL, Aug. 2021;              *
+* Coded for CMPE 163 from the reference link below for Ubuntu Linux        *
+* Compilation and link:                                                    * 
+* g++ hello-opengl.cpp -o firstOpenGlApp -lglut -lGLU -lGL                 * 
+* http://www.codebind.com/linux-tutorials/install-opengl-ubuntu-linux/     *
+* -------------------------------------------------------------------------* 
+*/
+
+#include <GL/glut.h>
+
+void displayMe(void)
+{
+    glClear(GL_COLOR_BUFFER_BIT);
+    glBegin(GL_POLYGON);
+        glVertex3f(0.5, 0.0, 0.5);
+        glVertex3f(0.5, 0.0, 0.0);
+        glVertex3f(0.0, 0.5, 0.0);
+        glVertex3f(0.0, 0.0, 0.5);
+    glEnd();
+    glFlush();
+}
+
+int main(int argc, char** argv)
+{
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_SINGLE);
+    glutInitWindowSize(400, 300);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("Hello world!");
+    glutDisplayFunc(displayMe);
+    glutMainLoop();
+    return 0;
+}
